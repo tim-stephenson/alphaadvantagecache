1aabcb8dd0a7d1ff76aa8cf906191349e777c6f6
tim-stephenson <38039215+tim-stephenson@users.noreply.github.com>
2023-06-05 11:43:58 -0400

    got serivce working

--- src/get_treasury_data.rs
+++ src/get_treasury_data.rs
@@ -1,10 +1,12 @@
-#[path = "parse_treasury_data.rs"] mod parse_treasury_data;
-
-
 use axum::Json;
+use serde::Serialize;
+use std::{io::BufReader, collections::BTreeMap};
+use bytes::{Bytes};
+
+use xml::reader::{EventReader, XmlEvent};
+
 use chrono::{DateTime, Utc, Months};
 
-use parse_treasury_data::{parse_treasury_xml, DoubleError, TreasuryBillRates};
 use url::{Url, ParseError};
 
 const BASEURL : &str = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/pages/xml";
@@ -32,11 +34,7 @@ async fn make_request(date : DateTime<Utc>) -> Result<Json<TreasuryBillRates> , 
                     Ok(resp)=> {
                         match resp.bytes().await {
                             Ok(bytes) => {
-                                match parse_treasury_xml(bytes){
-                                    Ok(json) =>{ return Ok(json); }
-                                    Err(DoubleError::NoEntries) =>{ return Err(TreasuryBillDataError::NoEntries); }
-                                    Err(DoubleError::XMLParseError(e)) => {return Err(TreasuryBillDataError::XMLParseError(e)); }
-                                }
+                                return parse_treasury_xml(bytes);
                             }
                             Err(e) => {  return Err(TreasuryBillDataError::RequestError(e));  }
                         }                
@@ -60,9 +58,13 @@ pub async fn get_data() -> Result<Json<TreasuryBillRates> , TreasuryBillDataErro
             return Ok(json);
         }
         Err(TreasuryBillDataError::NoEntries) =>{
-            let x = Utc::now();
-            x.checked_sub_months(Months::new(1));
-            return make_request(x).await;
+            let curr_time = Utc::now();
+            match curr_time.checked_sub_months(Months::new(1)){
+                Some(one_month_ago) =>{ 
+                    return make_request(one_month_ago).await;
+                }
+                None => { return Err(TreasuryBillDataError::NoEntries) }
+            };
         }
         Err(e) => {
             return Err(e);
@@ -70,3 +72,88 @@ pub async fn get_data() -> Result<Json<TreasuryBillRates> , TreasuryBillDataErro
 
     }
 }
+
+
+
+
+
+
+const FIELD_VALUES : [&str; 7] = ["ROUND_B1_YIELD_4WK_2","ROUND_B1_YIELD_8WK_2","ROUND_B1_YIELD_13WK_2","ROUND_B1_YIELD_17WK_2","ROUND_B1_YIELD_26WK_2", "ROUND_B1_YIELD_52WK_2","INDEX_DATE"];
+
+#[derive(Debug,Serialize)]
+pub struct TreasuryBillRates{
+    ROUND_B1_YIELD_4WK_2 : String,
+    ROUND_B1_YIELD_8WK_2 : String,
+    ROUND_B1_YIELD_13WK_2 : String,
+    ROUND_B1_YIELD_17WK_2 : String,
+    ROUND_B1_YIELD_26WK_2 : String,
+    ROUND_B1_YIELD_52WK_2 : String,
+    INDEX_DATE : String
+}
+
+
+// From https://home.treasury.gov/resource-center/data-chart-center/interest-rates/TextView
+pub fn parse_treasury_xml(input : Bytes) -> Result<Json<TreasuryBillRates> , TreasuryBillDataError >{
+    let stream = BufReader::new(input.as_ref());
+    let parser = EventReader::new(stream);
+
+
+    let mut found_entry = false;
+    let mut current_catagory = String::new();
+    let mut rates = BTreeMap::new();
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                current_catagory = name.local_name;
+                if !found_entry && current_catagory.as_str() == "entry" {
+                    found_entry = true;
+                }
+            }
+            Ok(XmlEvent::Characters(s) ) => {
+                if FIELD_VALUES.contains( &current_catagory.as_str()   ) {
+                    rates.insert(current_catagory.clone(), s);
+                }
+            }
+            Err(e) => {
+                return Err(TreasuryBillDataError::XMLParseError(e));
+            }
+            // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
+            _ => {}
+        }
+    }
+    if !found_entry{
+        return Err(TreasuryBillDataError::NoEntries);
+    }
+    let mut rates_struct = TreasuryBillRates{
+        ROUND_B1_YIELD_4WK_2 : String::new(),
+        ROUND_B1_YIELD_8WK_2 : String::new(),
+        ROUND_B1_YIELD_13WK_2 : String::new(),
+        ROUND_B1_YIELD_17WK_2 : String::new(),
+        ROUND_B1_YIELD_26WK_2 : String::new(),
+        ROUND_B1_YIELD_52WK_2 : String::new(),
+        INDEX_DATE : String::new()
+    };
+    if let Some(v) = rates.get("ROUND_B1_YIELD_4WK_2"){
+        rates_struct.ROUND_B1_YIELD_4WK_2 = v.to_string();
+    }
+    if let Some(v) = rates.get("ROUND_B1_YIELD_8WK_2"){
+        rates_struct.ROUND_B1_YIELD_8WK_2 = v.to_string();
+    }
+    if let Some(v) = rates.get("ROUND_B1_YIELD_13WK_2"){
+        rates_struct.ROUND_B1_YIELD_13WK_2 = v.to_string();
+    }
+    if let Some(v) = rates.get("ROUND_B1_YIELD_17WK_2"){
+        rates_struct.ROUND_B1_YIELD_17WK_2 = v.to_string();
+    }
+    if let Some(v) = rates.get("ROUND_B1_YIELD_26WK_2"){
+        rates_struct.ROUND_B1_YIELD_26WK_2 = v.to_string();
+    }
+    if let Some(v) = rates.get("ROUND_B1_YIELD_52WK_2"){
+        rates_struct.ROUND_B1_YIELD_52WK_2 = v.to_string();
+    }
+    if let Some(v) = rates.get("INDEX_DATE"){
+        rates_struct.INDEX_DATE = v.to_string();
+    }
+    return Ok( Json(rates_struct) );
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 mod get_treasury_data;
-
+use get_treasury_data::TreasuryBillRates;
 
 use axum::{
-    routing::{get, post},
-    http::{StatusCode, Response},
-    response::IntoResponse,
+    routing::{get},
+    http::{StatusCode},
     Json, Router,
-    extract::Path
 };
-use bytes::{Bytes, Buf};
-use serde::{Deserialize, Serialize};
+
 use std::{net::SocketAddr};
 
-use std::time::{Duration, SystemTime};
+
 
 
 
@@ -27,7 +24,7 @@ async fn main() {
 
     // build our application with a route
     let app = Router::new()
-        .route("/treasury_bill_rates", get());
+        .route("/treasury_bill_rates", get(treasury_bill_rates_handler) );
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -39,43 +36,11 @@ async fn main() {
         .unwrap();
 }
 
-treasury_bill_rates
 
 
-
-
-
-
-
-
-// basic handler that responds with a static string
-// async fn root() -> &'static str {
-//     "Hello, World!"
-// }
-
-
-
-
-
-
-
-// async fn get_market_index(Path(symbol): Path<String>) -> Json<MarketIndex>{
-//     println!("GET request for /market_index/:symbol for symbol: {}", symbol);
-//     return Json(MarketIndex { 
-//         symbol: "spx".to_string(), 
-//         full_name: "S&P 500".to_string(), 
-//         updated: 1685477607620, 
-//         value: 4536.5, 
-//         percent_change: -1.5 }) ;
-// }
-
-
-// #[derive(Serialize)]
-// struct MarketIndex {
-//     symbol : String,
-//     full_name : String,
-//     updated : u64,
-//     value : f32,
-//     percent_change : f32,
-
-// }
+async fn treasury_bill_rates_handler() -> Result<Json<TreasuryBillRates>, StatusCode> {
+    match get_treasury_data::get_data().await {
+        Ok(json) => Ok(json),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
